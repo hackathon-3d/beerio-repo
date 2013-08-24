3aec946b20d464731764bde205b96d0eff9f3ccc
Tony Winters <tony.winters@m33integrated.com>
2013-08-23 23:41:20 -0400

    UI Changes

---
+++ Beer.io/LandingViewController.h
@@ -0,0 +1,13 @@
+//
+//  LandingViewController.h
+//  Beer.io
+//
+//  Created by Tony Winters on 8/23/13.
+//  Copyright (c) 2013 Tony Winters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LandingViewController : UIViewController
+
+@end
---
+++ Beer.io/LandingViewController.m
@@ -0,0 +1,38 @@
+//
+//  LandingViewController.m
+//  Beer.io
+//
+//  Created by Tony Winters on 8/23/13.
+//  Copyright (c) 2013 Tony Winters. All rights reserved.
+//
+
+#import "LandingViewController.h"
+
+@interface LandingViewController ()
+
+@end
+
+@implementation LandingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
