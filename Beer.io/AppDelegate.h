7d183657832ddf00f24a95b16297e4186999efc5
Tony Winters <tony.winters@m33integrated.com>
2013-08-23 21:10:35 -0400

    Initial Commit Bitches! BEER!

---
+++ Beer.io/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Beer.io
+//
+//  Created by Tony Winters on 8/23/13.
+//  Copyright (c) 2013 Tony Winters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Beer.io/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  Beer.io
+//
+//  Created by Tony Winters on 8/23/13.
+//  Copyright (c) 2013 Tony Winters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
